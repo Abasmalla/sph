57fa45116a33a1af7dd39f2f09b878a2e4adc37a
Abas Mushtaq <43537057+Abasmalla@users.noreply.github.com>
2024-04-13 00:05:19 +0530

    Update dbconn.php

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -1,9 +1,9 @@
 <?php
 /* Database config */
-$db_host		= 'localhost';
-$db_user		= 'id19905563_abasmalla';
-$db_pass		= '@Abas3212206@';
-$db_database	= 'id19905563_shoppet'; 
+$db_host		= 'us-cluster-east-01.k8s.cleardb.net';
+$db_user		= 'bd52157b8a0ec4';
+$db_pass		= '663ea7a3';
+$db_database	= 'heroku_f460bdcbafa30cf'; 
 /* End config */
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_database);
